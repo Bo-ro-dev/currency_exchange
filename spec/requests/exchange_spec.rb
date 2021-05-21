b5da8e7336d207114af099f5c4d8680036b0ef12
bo-ro-dev <adam.borowiec.2000@gmail.com>
2021-05-21 15:36:24 +0200

    Add exchange tests

--- spec/requests/exchange_spec.rb
+++ spec/requests/exchange_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Exchanges", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+
+  describe "GET /show" do
+    context "works fine" do
+      it "returns HTTP 200" do
+        get "/show"
+        expect(response).to have_http_status(200)
+      end
+
+      it "do not set @rel_amount" do
+        get "/show"
+        expect(@rel_amount).to be_nil
+      end
+    end
   end
 end
---
+++ spec/views/exchange/show_spec.rb
@@ -0,0 +1,11 @@
+# describe "views/exchange/show.html.erb" do
+
+#   describe "@rel_amount" do
+#     context "ask for price" do
+#       it "exists" do
+#         get "/show"
+#         expect(@rel_amount).to be_nil
+#       end
+#     end
+#   end
+# end
