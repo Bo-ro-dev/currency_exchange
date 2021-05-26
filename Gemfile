1c0f870202c19299f44de411a95b88f748c4c276
bo-ro-dev <adam.borowiec.2000@gmail.com>
2021-05-26 11:59:33 +0200

    heroku deploy

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'webmock'
 end
--- spec/ratio_fetcher_spec.rb
+++ spec/ratio_fetcher_spec.rb
@@ -2,6 +2,14 @@ describe "RatioFetcher" do
   describe "#request_for" do
     context "fetch the ratio" do
       it "returns ratio" do
+        stub_request(:get, "https://free.currconv.com/api/v7/convert?apiKey=909106a1404e597b35f4&compact=ultra&q=PLN_USD,USD_PLN").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.4.1'
+           }).
+         to_return(status: 200, body: "{\"PLN_USD\":0.270855,\"USD_PLN\":3.692015}", headers: {})
 
         expect(RatioFetcher.request_for("PLN","USD")).to be_a_kind_of(Float)
       end
