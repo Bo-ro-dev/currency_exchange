0cd04077a993e5d96a47c945b5533609d4fba070
bo-ro-dev <adam.borowiec.2000@gmail.com>
2021-05-26 12:56:13 +0200

    hotfix2

--- app/lib/ratio_fetcher.rb
+++ app/lib/ratio_fetcher.rb
@@ -2,6 +2,6 @@ class RatioFetcher
   def self.request_for(ref, rel)
     request = Faraday.get "https://free.currconv.com/api/v7/convert?q=#{ref}_#{rel},#{rel}_#{ref}&compact=ultra&apiKey=909106a1404e597b35f4"
     data = JSON.parse(request.body)
-    ratio = data[rel+"_"+ref]
+    ratio = data[ref+"_"+rel]
   end
 end
